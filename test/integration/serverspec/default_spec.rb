ec5340fe88cb9f0884fc20b3fd33b03d8304c7d3
Christian Witts <cwitts@gmail.com>
2018-10-03 10:09:31 +0200

    📦 Add conformance tests
    Signed-off-by: Christian Witts <cwitts@gmail.com>

--- test/integration/serverspec/default_spec.rb
+++ test/integration/serverspec/default_spec.rb
@@ -1,5 +1,32 @@
 require_relative '../spec_helper'
 
-describe 'nat-gateway' do
-  it 'should do something'
+describe package('nginx'), :if => os[:family] == 'redhat' do
+  it { should_be_installed }
+end
+
+describe package('squid'), :if => os[:family] == 'redhat' do
+  it { should_be_installed }
+end
+
+describe service('nginx'), :if => os[:family] == 'redhat' do
+  it { should_be_enabled }
+  it { should_be_running }
+end
+
+describe service('squid'), :if => os[:family] == 'redhat' do
+  it { should_be_enabled }
+  it { should_be_running }
+end
+
+describe service('node-exporter'), :if => os[:family] == 'redhat' do
+  it { should_be_enabled }
+  it { should_be_running }
+end
+
+describe port(3128) do
+  it { should_be_listening }
+end
+
+describe port(9090) do
+  it { should_be_listening }
 end
